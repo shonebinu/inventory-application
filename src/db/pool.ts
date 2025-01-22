39b1f39c14d2ab5641c966027e30a36930698589
Shone Binu <62597277+shonebinu@users.noreply.github.com>
2025-01-22 21:54:49 +0530

    project structure and db scripts

---
+++ src/db/pool.ts
@@ -0,0 +1,8 @@
+import "dotenv/config";
+import pg from "pg";
+
+const { Pool } = pg;
+
+export default new Pool({
+  connectionString: process.env.DB_URL,
+});
---
+++ src/db/populateDb.ts
@@ -0,0 +1,64 @@
+import pg from "pg";
+
+const { Client } = pg;
+
+const SQL = `
+CREATE TABLE IF NOT EXISTS book (
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  title VARCHAR(255) NOT NULL,
+  isbn VARCHAR(20) UNIQUE,
+  publication_date DATE,
+  description TEXT
+);
+
+CREATE TABLE IF NOT EXISTS author (
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  name VARCHAR(255) NOT NULL,
+  bio TEXT
+);
+
+CREATE TABLE IF NOT EXISTS genre (
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  name VARCHAR(255) NOT NULL UNIQUE,
+  description TEXT
+);
+
+CREATE TABLE IF NOT EXISTS book_author (
+  book_id INTEGER REFERENCES book(id) ON DELETE CASCADE,
+  author_id INTEGER REFERENCES author(id) ON DELETE CASCADE,
+  PRIMARY KEY(book_id, author_id)
+);
+
+CREATE TABLE IF NOT EXISTS book_genre (
+  book_id INTEGER REFERENCES book(id) ON DELETE CASCADE,
+  genre_id INTEGER REFERENCES genre(id) ON DELETE CASCADE,
+  PRIMARY KEY(book_id, genre_id)
+);
+`;
+
+const connectionString = process.argv[2];
+
+if (!connectionString) {
+  console.error("Error: Please provide a connection URL as an argument.");
+  console.log(
+    "Connection URL example: postgresql://<role_name>:<role_password>@<db_host>:5432/<db_name>",
+  );
+  process.exit(1);
+}
+
+console.log("Seeding...");
+
+const client = new Client({
+  connectionString,
+  ssl: { rejectUnauthorized: false },
+});
+
+try {
+  await client.connect();
+  await client.query(SQL);
+  console.log("Database seeded successfully.");
+} catch (error) {
+  console.log("Error seeding the database:", error);
+} finally {
+  await client.end();
+}
